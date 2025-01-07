699614a42c76207b4ce225b38a4904eb857c5082
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-01-07 16:39:29 +0000

    Updated task 0

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,12 @@
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // React 18+ render method
+import App from './App'; // Import the App component
+import './index.css'
 
-// Render the App component to the root element
+
+// Get the root element from the HTML
+const rootElement = document.getElementById('root');
+
+// Create the React root and render the App component
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
