eedaeb2b5ea1467e40905bf438392bf8ce1fc0d9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-01-11 17:54:15 +0000

    updated image, and image import

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,20 +1,20 @@
-// src/components/AboutMe.jsx
+// src/components/AboutMe/AboutMe.jsx
 import React from "react";
-
+import ManningLinkedInImage from "../../assets/manning-linkedin.jpeg"; // Import the image
 
 const AboutMe = () => {
   return (
     <div className="about-me">
       <img
-        src="./assets/manning-linkedin.jpeg" // Replace with your actual image URL
-        alt="Manning Stinson Professinal Photo"
+        src={ManningLinkedInImage} // Use the imported image
+        alt="Manning Stinson Professional Photo"
         className="about-me-image"
       />
       <p>
         Hi, my name is Manning. I am a Full Stack student in my 5th trimester.
-        My favorite languages and technologies to work with are Python,React, HTML, CSS, Javascript.
+        My favorite languages and technologies to work with are Python, React, HTML, CSS, JavaScript.
         After school, I hope to pursue a career in Full Stack Development.
-        Outside of school, I enjoy Reading, Writing, Bloggin, and I am passionate about teaching adults how to swim.
+        Outside of school, I enjoy Reading, Writing, Blogging, and I am passionate about teaching adults how to swim.
       </p>
     </div>
   );
